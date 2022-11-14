9c220cc103137bc37a3a06a5e6358dee1c6ea6ce
quangneyyy <quang.phanvinh@meowhouse.vn>
2022-11-14 21:31:21 +0700

    5 create-new-user

--- src/routes/web.js
+++ src/routes/web.js
@@ -10,6 +10,7 @@ const router = express.Router();
 const initWebRoutes = (app) => {
   router.get("/", homeController.handleHelloWorld);
   router.get("/user", homeController.handleUserPage);
+  router.post("/users/create-user", homeController.handleCreateNewUser);
 
   return app.use("/", router);
 };
