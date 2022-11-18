c521f67734c8cfc948d518d1f6cc7e20f96c67db
quangneyyy <quang.phanvinh@meowhouse.vn>
2022-11-18 16:43:23 +0700

    6.1-generate-model/migration/seeder

---
+++ src/migrations/20221118023246-create-user.js
@@ -0,0 +1,25 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      username: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Users");
+  },
+};
---
+++ src/seeders/20221118075011-demo-user.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          email: "quang@gmail.com",
+          password: "123456",
+          username: "John Doe",
+        },
+        {
+          email: "ney@gmail.com",
+          password: "123456",
+          username: "John Doe",
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
