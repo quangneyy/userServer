3c43ea13d444c00a29e3214f99347584bc592087
quangneyyy <quang.phanvinh@meowhouse.vn>
2022-11-24 11:39:46 +0700

    9.4.2-mock-apis

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,7 @@
 import express from "express";
 import homeController from "../controller/homeController";
+import apiController from "../controller/apiController";
+
 const router = express.Router();
 
 /**
@@ -14,6 +16,10 @@ const initWebRoutes = (app) => {
   router.post("/delete-user/:id", homeController.handleDeleteUser);
   router.get("/update-user/:id", homeController.getUpdateUserPage);
   router.post("/user/update-user", homeController.handleUpdateUser);
+
+  //rest api
+  //GET - R, POST - C, PUT - U, DELETE - D
+  router.get("/api/test-api", apiController.testApi);
 
   return app.use("/", router);
 };
