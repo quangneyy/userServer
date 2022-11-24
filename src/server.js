05d5582cfc805aa43c41abdadf2ca0f0b453ae2e
quangneyyy <quang.phanvinh@meowhouse.vn>
2022-11-24 22:36:22 +0700

    9.4.3-fix-cors

--- src/server.js
+++ src/server.js
@@ -8,6 +8,31 @@ import connection from "./config/connectDB";
 const app = express();
 const PORT = process.env.PORT || 8081;
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", process.env.REACT_URL);
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // config view engine
 configViewEngine(app);
 
