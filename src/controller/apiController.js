f788d3bedc61775f09b1722b09035023fbf4cdea
quangneyyy <quang.phanvinh@meowhouse.vn>
2022-11-26 15:11:06 +0700

    10.3.1-login-form

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -42,7 +42,16 @@ const handleRegister = async (req, res) => {
   }
 };
 
+const handleLogin = async (req, res) => {
+  console.log(">>> check login from react: ", req.body);
+  return res.status(200).json({
+    message: "ok",
+    data: "test api login",
+  });
+};
+
 module.exports = {
   testApi,
   handleRegister,
+  handleLogin,
 };
--- src/routes/api.js
+++ src/routes/api.js
@@ -13,7 +13,7 @@ const initApiRoutes = (app) => {
   //GET - R, POST - C, PUT - U, DELETE - D
   router.get("/test-api", apiController.testApi);
   router.post("/register", apiController.handleRegister);
-
+  router.post("/login", apiController.handleLogin);
   return app.use("/api/v1/", router);
 };
 
