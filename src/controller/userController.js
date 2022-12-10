bd7200c2dbb6ccf262881124178d66f9f3b14e32
quangneyyy <quang.phanvinh@meowhouse.vn>
2022-12-10 15:59:59 +0700

    10.6.3-delete-users

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -55,14 +55,20 @@ const updateFunc = (req, res) => {
   }
 };
 
-const deleteFunc = (req, res) => {
+const deleteFunc = async (req, res) => {
   try {
+    let data = await userApiService.deleteUser(req.body.id);
+    return res.status(200).json({
+      EM: data.EM, // error message
+      EC: data.EC, // error code
+      DT: data.DT, // data
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
       EM: "error from server", // error message
       EC: "-1", // error code
-      DT: "", // date
+      DT: "", // data
     });
   }
 };
--- src/service/userApiService.js
+++ src/service/userApiService.js
@@ -37,6 +37,8 @@ const getUserWithPagination = async (page, limit) => {
     const { count, rows } = await db.User.findAndCountAll({
       offset: offset,
       limit: limit,
+      attributes: ["id", "username", "email", "phone", "sex"],
+      include: { model: db.Group, attributes: ["name", "description"] },
     });
 
     let totalPages = Math.ceil(count / limit);
@@ -91,11 +93,30 @@ const updateUser = async (data) => {
 
 const deleteUser = async (id) => {
   try {
-    await db.User.deleteUser({
+    let user = await db.User.findOne({
       where: { id: id },
     });
+    if (user) {
+      await user.destroy();
+      return {
+        EM: "Delete user succeeds",
+        EC: 0,
+        DT: [],
+      };
+    } else {
+      return {
+        EM: "User not exist",
+        EC: 2,
+        DT: [],
+      };
+    }
   } catch (e) {
     console.log(e);
+    return {
+      EM: "Error from service",
+      EC: 1,
+      DT: [],
+    };
   }
 };
 
