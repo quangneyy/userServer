34cf7fc18bb74bfd6bb50164c919a43f0f7b6d4a
quangneyyy <quang.phanvinh@meowhouse.vn>
2022-12-14 14:28:44 +0700

    10.6.4-modal-create-user

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,7 @@
 import express from "express";
 import apiController from "../controller/apiController";
 import userController from "../controller/userController";
+import groupController from "../controller/groupController";
 const router = express.Router();
 
 /**
@@ -20,6 +21,8 @@ const initApiRoutes = (app) => {
   router.post("/user/create", userController.createFunc);
   router.put("/user/update", userController.updateFunc);
   router.delete("/user/delete", userController.deleteFunc);
+
+  router.get("/group/read", groupController.readFunc);
 
   return app.use("/api/v1/", router);
 };
